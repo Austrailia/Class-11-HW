ab764b94f37f2ca9200883fdc54581eac709bcb6
Connecting69 <austinli823@gmail.com>
2021-08-20 11:27:59 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  let [sumOfNumbers, setSumOfNumbers] = useState();
+
+  const onChange1 = (a) => setNum1(a.target.value);
+
+  const onChange2 = (a) => setNum2(a.target.value);
+
+  const doAddition = () => setSumOfNumbers((+num1) + (+num2));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Adding Two Numbers</h1>
+      <div>
+        <input value={num1} type="number" onChange={onChange1}/>
+        <input value={num2} type="number" onChange={onChange2}/>
+      </div>
+      <button onClick={doAddition}>Add Them!</button>
+      <h1 className="sumOfNumbers">{isNaN(sumOfNumbers) ? (sumOfNumbers = 0) : sumOfNumbers}</h1>
     </div>
   );
 }
